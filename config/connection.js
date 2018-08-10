8d3340c9b10de24d6f1cfdc92ea46d7d6c74649e
Nick Antourakis <35820875+nantourakis@users.noreply.github.com>
2018-08-10 17:26:16 -0400

    Add files via upload

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Chicken11!',
+    database: 'burgers_db'
+})
+
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect(function(err){
+    if (err)throw err;
+    console.log("Connected as id: " + connection.threadId);
+});
+
+module.exports = connection;
