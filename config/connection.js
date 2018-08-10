07a4f6057d34827d52434636c145d446a97b6f93
Nick Antourakis <35820875+nantourakis@users.noreply.github.com>
2018-08-10 17:30:13 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Chicken11!',
+    password: '',
     database: 'burgers_db'
 })
 
